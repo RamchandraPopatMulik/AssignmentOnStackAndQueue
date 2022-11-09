a3a65c1b872576b30215a3ec8b6e0d519a8582ea
RamchandraPopatMulik <mulikram2016@gmail.com>
2022-11-09 15:10:26 +0530

    Added file

--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -23,6 +23,12 @@
             queue.Enqueue(30);
             queue.Enqueue(70);
             queue.display();
+            queue.Dequeue();
+            queue.display();
+            queue.Dequeue();
+            queue.display();
+            queue.Dequeue();
+            queue.display();
         }
     }
 }
